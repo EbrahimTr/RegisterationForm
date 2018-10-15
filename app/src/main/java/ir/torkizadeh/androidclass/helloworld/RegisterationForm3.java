3076c1e2ab6c7ed4c1df0d0434419b17f8b0ee9b
EbrahimTr <programming.ir@gmail.com>
2018-10-16 02:17:08 +0330

    :wq
    :wq

---
+++ app/src/main/java/ir/torkizadeh/androidclass/helloworld/RegisterationForm3.java
@@ -0,0 +1,13 @@
+package ir.torkizadeh.androidclass.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterationForm3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registeration_form3);
+    }
+}
